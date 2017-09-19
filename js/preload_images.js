2a1eb959271bd12c067009d8bebdae979153a086
HarryStevens <harryjosephstevens@gmail.com>
2017-09-19 19:07:09 +0530

    Better image preloading

--- js/preload_images.js
+++ js/preload_images.js
@@ -1,15 +1,38 @@
 d3.json("data/tiles.json", function(error, data){
 	if (error) throw error;
 
-	data.forEach(function(img){
+	data.forEach(function(img, index){
 
-		preloadImage("img/tiles/" + img);
+		addLoadEvent(preloader("img/tiles/" + img, index));
+		// preloadImage();
 
 	});
 
 });
 
-function preloadImage(url){
-  var img = new Image();
-  img.src = url;
+// function preloadImage(url){
+
+//   var img = new Image();
+//   img.src = url;
+// }
+
+// better image preloading @ https://perishablepress.com/press/2009/12/28/3-ways-preload-images-css-javascript-ajax/
+function preloader(url, index) {
+	$("body").append("<div id='preload-" + index + "'></div>")
+	if (document.getElementById) {
+		document.getElementById("preload-" + index).style.background = "url(" + url + ") no-repeat -9999px -9999px";
+	}
+}
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function() {
+			if (oldonload) {
+				oldonload();
+			}
+			func();
+		}
+	}
 }
