a3e53192ca21167d807d5b281655135705335a14
HarryStevens <harryjosephstevens@gmail.com>
2017-09-21 16:30:58 +0530

    Add more image tiles

--- js/preload_images.js
+++ js/preload_images.js
@@ -1,9 +1,20 @@
+d3.json("data/first_tiles.json", function(error, data){
+	if (error) throw error;
+
+	data.forEach(function(img, index){
+
+		preloader("img/tiles/" + img, index);
+		
+	});
+
+});
+
 d3.json("data/tiles.json", function(error, data){
 	if (error) throw error;
 
 	data.forEach(function(img, index){
 
-		addLoadEvent(preloader("img/tiles/" + img, index));
+		preloader("img/tiles/" + img, index);
 		
 	});
 
@@ -11,11 +22,12 @@ d3.json("data/tiles.json", function(error, data){
 
 // better image preloading @ https://perishablepress.com/press/2009/12/28/3-ways-preload-images-css-javascript-ajax/
 function preloader(url, index) {
-	$("body").append("<div id='preload-" + index + "'></div>")
+	$("body").append("<div id='preload-" + index + "'></div>");
 	if (document.getElementById) {
 		document.getElementById("preload-" + index).style.background = "url(" + url + ") no-repeat -9999px -9999px";
 	}
 }
+
 function addLoadEvent(func) {
 	var oldonload = window.onload;
 	if (typeof window.onload != 'function') {
